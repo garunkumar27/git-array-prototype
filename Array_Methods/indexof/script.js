2c3d8eff2a580a9043b8b6ef43c4b8509713b0c3
arun kumar <garunk.27@gmail.com>
2022-08-20 12:05:02 +0530

    array Prototype.

---
+++ Array_Methods/indexof/script.js
@@ -0,0 +1,14 @@
+// indexof __to find the value using index number
+
+let a=[27,30,02,22];// variable declaration and definition & it is an array variable
+
+console.log(a[2]);// print the value using index value
+
+let b=a.indexOf(22);// Find the index number by giving the value present in an array
+
+console.log(b);// Printing statement
+
+// Similarly for string and negative numbers.
+// If the given value is not present in the array then it show (-1) as an output
+
+
