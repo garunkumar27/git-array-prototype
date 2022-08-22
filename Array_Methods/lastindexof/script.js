be58ec312c5d708cf33fc9ae4b30726dd59c4a45
arun kumar <garunk.27@gmail.com>
2022-08-22 09:59:02 +0530

    array Prototype.

---
+++ Array_Methods/lastindexof/script.js
@@ -0,0 +1,9 @@
+// It helps to take the last index number of the given value
+// Repititive times a value can repeat but it only take the last repetition of the value 
+let a=[1,5,8,9,3,2,4,5,2,8,7,2,1,3,5,1];
+b=a.lastIndexOf(3);
+console.log(b);
+
+// output
+// 13
+// lastIndex value of 3 is 13
