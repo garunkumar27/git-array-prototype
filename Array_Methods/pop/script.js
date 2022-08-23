1e2f1eeb7937ae22f26473e2afff7e7d620502f5
arun kumar <garunk.27@gmail.com>
2022-08-23 14:57:01 +0530

    array Prototype.

---
+++ Array_Methods/pop/script.js
@@ -0,0 +1,11 @@
+// Check index.html for definition
+let a=["Arun","Kumar","Ak","Family"];
+a.pop();
+console.log(a);
+
+// output
+// (3) ['Arun', 'Kumar', 'Ak']
+// 0: "Arun"
+// 1: "Kumar"
+// 2: "Ak"
+// length: 3
