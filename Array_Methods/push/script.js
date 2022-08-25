5ecc50a4e58c8cec015139afe404b5e725042372
arun kumar <garunk.27@gmail.com>
2022-08-25 09:50:04 +0530

    Array Prototype.

---
+++ Array_Methods/push/script.js
@@ -0,0 +1,20 @@
+// push__It is used to add one value in array
+
+let a=["Nura","Ramuk","Eerhs","Ahpur"];
+a.push("Illav");//adding one array value 
+console.log(a);
+
+// Array(5)
+// 0: "Nura"
+// 1: "Ramuk"
+// 2: "Eerhs"
+// 3: "Ahpur"
+// 4: "Illav"
+// length: 5
+
+let b=[1,2,3,4,5];
+b.push(6,7);
+console.log(b);
+
+// Output
+ [1, 2, 3, 4, 5, 6, 7]//Adding More than one value is possible
