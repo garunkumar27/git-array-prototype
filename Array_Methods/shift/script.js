c5fa3b3e88dccadc308fdf74f948ffc6532df2b3
arun kumar <garunk.27@gmail.com>
2022-08-25 16:56:59 +0530

    Array Prototype.

---
+++ Array_Methods/shift/script.js
@@ -0,0 +1,12 @@
+// push
+// Remove the first (a[0]) value in array 
+let a=["Nura","Ramuk","Eerhs","Ahpur"];
+a.shift();
+console.log(a);
+
+// output
+// (3) ['Ramuk', 'Eerhs', 'Ahpur']
+// 0: "Ramuk"
+// 1: "Eerhs"
+// 2: "Ahpur"
+// length: 3
