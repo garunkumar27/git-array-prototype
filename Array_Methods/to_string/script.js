cce469d86157cf056fb60330a443e73fd6759d48
arun kumar <garunk.27@gmail.com>
2022-09-02 09:46:29 +0530

    Array Prototype.

---
+++ Array_Methods/to_string/script.js
@@ -0,0 +1,11 @@
+// Converting number to string
+
+let a=[2,4,6,8,10];
+let b=a.toString();
+console.log(b);
+console.log(typeof b); //Checking whether it changed to string type or not(typeof)
+
+//output
+// 2,4,6,8,10
+// String
+
