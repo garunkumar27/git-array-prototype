a3ab6c441cb6077043f88ea3b5a29e8c5752b716
arun kumar <garunk.27@gmail.com>
2022-09-03 09:03:12 +0530

    Array Prototype.

---
+++ Array_Methods/unshift/script.js
@@ -0,0 +1,12 @@
+// unshift helps to add the value in a[0] index.
+let a=["Arun",'Kumar',"Mr.Ganesan"]
+a.unshift('G');
+console.log(a);
+
+// output
+// Array(4)
+// 0: "G"
+// 1: "Arun"
+// 2: "Kumar"
+// 3: "Mr.Ganesan"
+// length: 4
